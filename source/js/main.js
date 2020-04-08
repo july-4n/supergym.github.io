6d2ba4bb5b3a1e31a3b576b342b1bd867cd6907f
July-4n <september4n@gmail.com>
2020-04-08 13:48:29 +0500

    начало верстки блоков проекта

---
+++ source/js/main.js
@@ -0,0 +1,29 @@
+// // eslint-disable-next-line strict
+// $('.coaches-slider').slick({
+//   // dots: true,
+//   arrows: true,
+//   infinite: true,
+//   // fade: true,
+//   cssEase: 'linear',
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+//   adaptiveHeight: true,
+//   responsive:[
+//     {
+//       breakpoint: 1199,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//       }
+//     },
+//     {
+//       breakpoint: 767,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//       }
+//     },
+
+//   ],
+// });
+$('.coaches-slider').slick();
